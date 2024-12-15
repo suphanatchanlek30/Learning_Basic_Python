91a2583d15ae3d7b81f079f86fd3f652a4c8ef09
suphanatchanlek <suphanatchanlek@gmail.com>
2024-12-15 23:42:55 +0700

    add Dictionary_and_Boolean

---
+++ Dictionary_and_Boolean/Dictionary_and_Boolean.py
@@ -0,0 +1,14 @@
+# Dictionary จะมี key และ value
+
+# key คือชื่อของตัวแปร
+# value คือค่าของตัวแปร
+
+# สร้าง Dictionary
+my_dict = {'key1': 10, 'key2': 'value2', 'key3': 'value3'}
+print(my_dict)
+
+print(my_dict['key1']) # output = 10
+
+# Boolean คือค่าจริงหรือไม่จริง
+# True = 1
+# False = 0
