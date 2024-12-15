f2dbe26f7feb72e946d99f71495d1d83358321de
suphanatchanlek <suphanatchanlek@gmail.com>
2024-12-15 23:25:33 +0700

    add Variable_and_Print

---
+++ Variable_and_Print/Variable_and_Print.py
@@ -0,0 +1,32 @@
+# Variable คือ ตัวแปร
+
+# int
+x = 1
+y = 2
+print(x + y)
+
+z = x + y
+print(z)
+
+# float
+x = 1.0
+y = 2.0
+print(x + y)
+
+z = x + y
+print(z)
+
+# string
+x = "Hello"
+y = "World"
+print(x + y)
+# 'single' , "d" คือ string
+
+# Print คำสั่งที่ใช้แสดงข้อมูล
+name = "Suphanat Chanlek"
+print("Hello, my name is {}".format(name))
+
+age = 13
+print("Hello, my name is {} and I'm {} years old".format(name, age))
+
+print("Hello, my name is {i} and I'm {o} years old".format(i = name, o = age))
